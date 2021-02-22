2a5953ff8e69360657cb20d6db19aec10f8adf8e
fretessema <fretessema@gmail.com>
2021-02-22 15:55:32 -0500

    First Commit

---
+++ RandomCircles.java
@@ -0,0 +1,24 @@
+/*Dear Srdan, I am still working on this*/
+
+
+
+/*Write a GraphicsProgram that draws a set of ten circles with different sizes, positions,
+and colors. Each circle should have a randomly chosen color, a randomly chosen radius
+between 5 and 50 pixels, and a randomly chosen position on the canvas, subject to the
+condition that the entire circle must fit inside the canvas without extending past the edge. */
+
+
+package week2;
+
+import acm.program.GraphicsProgram;
+
+public class RandomCircles extends GraphicsProgram {
+
+	private static final double MIN_Radius = 5;
+	
+	private static final double MAX_Radius = 50;
+	
+	private static final int Number_Circles = 10;
+	
+	
+}
